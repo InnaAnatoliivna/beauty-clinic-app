db7e28d29d225f9e3661fe3874491a72688e9a2d
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2024-10-01 22:38:34 +0200

    fix header styles

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -43,23 +43,23 @@ export const MenuIconStyled = styled(CiMenuBurger)`
 `;
 
 export const LinkStyled = styled(Link)`
-text-align: center;
-        font-size: 14px;
-        font-weight: 400;
-        line-height: 1.5;
-        padding: 10px 27px;
-        color: var(--color-black);
-        border: 1px solid var(--color-main-text);
-        box-shadow: 0px 2px 1px rgba(46, 47, 66, 0.08), 0px 1px 1px rgba(46, 47, 66, 0.16), 0px 1px 6px rgba(46, 47, 66, 0.08);
-        border-radius: 12px;
+    text-align: center;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1.5;
+    padding: 10px 27px;
+    color: var(--color-black);
+    border: 1px solid var(--color-main-text);
+    box-shadow: 0px 2px 1px rgba(46, 47, 66, 0.08), 0px 1px 1px rgba(46, 47, 66, 0.16), 0px 1px 6px rgba(46, 47, 66, 0.08);
+    border-radius: 12px;
 
-        &.active {
-            color: orange;
-        }
-        &:hover, :focus {
-            border: 1px solid var(--color-black);
-        }
-        a {
-            color: var(--color-main-text);
-        }
-`
+    &.active {
+        color: orange;
+    }
+    &:hover, :focus {
+        border: 1px solid var(--color-black);
+    }
+    a {
+        color: var(--color-main-text);
+    }
+`;
