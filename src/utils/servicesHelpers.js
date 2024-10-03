510a0160d043c16b16efc22bcc01679ea5b52a2d
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2024-10-04 00:03:36 +0200

    added images

--- src/utils/servicesHelpers.js
+++ src/utils/servicesHelpers.js
@@ -8,48 +8,76 @@ import dataEndermology from '../resources/dataServices/dataEndermology.json';
 import dataConsultation from '../resources/dataServices/dataConsultation.json';
 import dataCleaning from '../resources/dataServices/dataCleaning.json';
 import dataBeautyPlan from '../resources/dataServices/dataBeautyPlan.json';
-
+import imgCleaningFirst from '../images/services/Oczyszczanie-wodorowe.jpg';
+import imgCleaningSecond from '../images/services/Zabieg-pca-twarz.jpg';
+import imgMMicroFirst from '../images/services/Mezoterapia-mikroigłowa1.jpg';
+import imgMMicroSecond from '../images/services/mezmikro.jpg';
+import imgMezotFirst from '../images/services/Mezoterapia-mikroigłowa1.jpg';
+import imgMezotSecond from '../images/services/Mezoterapia-skóry-głowy.jpg';
+import imgStFirst from '../images/services/stymulator.jpg';
+import imgStSecond from '../images/services/stymulat.jpg';
+import imgStorzFirst from '../images/services/Fala-udzerzeniowa-Storz1.jpg';
+import imgStorzSecond from '../images/services/DSC01391.jpg';
+import imgEpilFirst from '../images/services/Epilacja.jpg';
+import imgEpilSecond from '../images/services/Epilacja-pach1.jpg';
+import imgEndermFirst from '../images/services/Enderm.jpg';
+import imgEndermSecond from '../images/services/Endermolif-twarz-szyja-dekolt1.jpg';
+import imgConsFirst from '../images/services/Masaż-twarZy.jpg';
+import imgConsSecond from '../images/services/Masaż-2.jpg';
+import imgKFirst from '../images/about-1.jpg';
+import imgKSecond from '../images/slides/slide8.jpg';
+import imgPlan from '../images/services/beauty.jpg';
 
 const services = [
     {
         id: "mezoterapia-mikroigłowa",
-        service: MesotherapyMicro
+        service: MesotherapyMicro,
+        image: [imgMMicroFirst, imgMMicroSecond]
     },
     {
         id: "mezoterapia-igłowa",
-        service: Mesotherapy
+        service: Mesotherapy,
+        image: [imgMezotFirst, imgMezotSecond]
     },
     {
         id: "stymulatory-tkankowe",
-        service: dataTissueStimulators
+        service: dataTissueStimulators,
+        image: [imgStFirst, imgStSecond]
     },
     {
         id: "storz-fala-uderzeniowa",
-        service: dataSTORZ
+        service: dataSTORZ,
+        image: [imgStorzFirst, imgStorzSecond]
     },
     {
         id: "epilacja-laserowa",
-        service: dataHairRemoval
+        service: dataHairRemoval,
+        image: [imgEpilFirst, imgEpilSecond]
     },
     {
         id: "podstawowa-pielęgnacja-twarzy",
-        service: dataFacialCare
+        service: dataFacialCare,
+        image: [imgConsFirst, imgConsSecond]
     },
     {
         id: "endermologia-lpg",
-        service: dataEndermology
+        service: dataEndermology,
+        image: [imgEndermFirst, imgEndermSecond]
     },
     {
         id: "konsultacja-przedzabiegowa",
-        service: dataConsultation
+        service: dataConsultation,
+        image: [imgKFirst, imgKSecond]
     },
     {
         id: "oczyszczanie-wodorowe",
-        service: dataCleaning
+        service: dataCleaning,
+        image: [imgCleaningFirst, imgCleaningSecond]
     },
     {
         id: "beauty-plan",
-        service: dataBeautyPlan
+        service: dataBeautyPlan,
+        image: [imgStorzSecond, imgPlan]
     }
 ];
 
